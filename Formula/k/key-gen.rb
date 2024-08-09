79784622a8c60c79852cd49fd5239f230644fd44
Evan Owen <Johnwowen3@gmail.com>
2024-08-08 23:48:47 -0400

    key-gen 0.0.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/k/key-gen.rb
+++ Formula/k/key-gen.rb
@@ -2,8 +2,8 @@ class KeyGen < Formula
   desc "key-gen is a CLI tool to generate keys for various blockchains"
   homepage "https://github.com/ulmentflam/key-gen"
   license "GPLv3"
-  url "https://github.com/ulmentflam/key-gen/releases/download/v0.0.1/key-gen-v0.0.1-darwin-arm64.tar.gz"
-  sha256 "ece24db297b3afea1dc560e69897bbedded429db2dd6f845fb4bfcf44f410c81"
+  url "https://github.com/ulmentflam/key-gen/releases/download/v0.0.2/key-gen-v0.0.2-darwin-arm64.tar.gz"
+  sha256 "8881d1bca3adb274a4a1de09e98765a9d0113f84f8992b107ca5c01325bd5c0b"
   def install
     bin.install "key-gen"
   end
